bb5eec8ce6a100ed078854bd7d7280faf9339bc7
Rayhona <raya0106777@gmail.com>
2026-04-14 23:23:33 +0500

    homework

--- task.go
+++ task.go
@@ -1,31 +1,14 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"strings"
-)
-
-func formatAmount(amount string) string {
-	var builder strings.Builder
-	n := len(amount)
-	for i, ch := range amount {
-		builder.WriteRune(ch)
-		if (n-i-1)%3 == 0 && i != n-1 {
-			builder.WriteRune(' ')
-		}
-	}
-	return builder.String()
-}
+import "fmt"
 
 func main() {
-	senderFlag := flag.String("sender", "", "имя отправителя")
-	amountFlag := flag.String("amount", "", "сумма")
-	flag.Parse()
-
-	sender := strings.ToUpper(*senderFlag)
-	amount := formatAmount(*amountFlag)
-
-	fmt.Printf("\n  %s сум поступили 🎉\n", amount)
-	fmt.Printf("  %s отправил вам деньги\n", sender)
+	sender := "Sherali"
+	receiver := "Alisher"
+	amount := 500000
+	isAzo := true
+	fmt.Printf("Sender: %s\n", sender)
+	fmt.Printf("Receiver: %s\n", receiver)
+	fmt.Printf("Amount: %d\n", amount)
+	fmt.Printf("Status: %t", isAzo)
 }
