6f95239cff2a14f99abbbe128d6d5a2395fb4901
Rayhona <raya0106777@gmail.com>
2026-04-17 00:17:50 +0500

    my changes

--- hw.go
+++ hw.go
@@ -1,6 +1,5 @@
 package main
 
-import "fmt"
-
 func Mask(number string) string {
-	return number[0:4] + " **** **** " + number[12:16]
+	return number[0:4] + " **** **** " + number[12:]
+}
--- hw_test.go
+++ hw_test.go
@@ -5,10 +5,11 @@ import (
 )
 
 func Example_mask() {
-	fmt.Println(Mask("****"))
+	fmt.Println(Mask("1233435436312"))
 	fmt.Println(Mask("8888888888888888"))
 	fmt.Println(Mask("abdsderfvderfswe"))
 	//Output:
+	//1233 **** **** 2
 	//
 	//8888 **** **** 8888
 	//abds **** **** fswe
